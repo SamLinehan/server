b8e29210e31f8f4617532a16f1093368e45b7de9
Samuel <samuel.linehan15@gmail.com>
2016-02-17 10:23:59 -0700

    migrating db to heroku

--- manage.py
+++ manage.py
@@ -1,9 +1,15 @@
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+from os.path import join, dirname
+from dotenv import load_dotenv
 import os
 
 from app import app, db
-app.config.from_pyfile('config.py')
+#Development
+# app.config.from_pyfile('config.py')
+#Production
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
