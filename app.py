4ba117dba523ba23aca0aaf6c3d6381c06208255
Samuel <samuel.linehan15@gmail.com>
2016-02-18 11:59:08 -0700

    testing flask-cors

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 from functools import update_wrapper
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
+from flask.ext.cors import CORS
 from os.path import join, dirname
 from dotenv import load_dotenv
 import flask.ext.restless
@@ -15,62 +16,17 @@ import json
 app = Flask(__name__, instance_relative_config=True)
 app.config['DEBUG'] = True
 heroku = Heroku(app)
+CORS(app)
 
 # api = restful.Api(app)
 
-# Development
 app.config.from_pyfile('config.py')
-# app.config.from_envvar('DATABASE_URL')
-# #Production
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 db = SQLAlchemy(app)
 
-# CORS
-
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            h['Access-Control-Allow-Credentials'] = 'true'
-            h['Access-Control-Allow-Headers'] = \
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
+# CORS Snippet
 
 # def crossdomain(origin=None, methods=None, headers=None,
 #                 max_age=21600, attach_to_all=True,
@@ -101,10 +57,12 @@ def crossdomain(origin=None, methods=None, headers=None,
 #                 return resp
 #
 #             h = resp.headers
-#
 #             h['Access-Control-Allow-Origin'] = origin
 #             h['Access-Control-Allow-Methods'] = get_methods()
 #             h['Access-Control-Max-Age'] = str(max_age)
+#             h['Access-Control-Allow-Credentials'] = 'true'
+#             h['Access-Control-Allow-Headers'] = \
+#                 "Origin, X-Requested-With, Content-Type, Accept, Authorization"
 #             if headers is not None:
 #                 h['Access-Control-Allow-Headers'] = headers
 #             return resp
@@ -112,7 +70,6 @@ def crossdomain(origin=None, methods=None, headers=None,
 #         f.provide_automatic_options = False
 #         return update_wrapper(wrapped_function, f)
 #     return decorator
-
 
 class User(db.Model):
     __tablename__ = "user"
@@ -157,19 +114,12 @@ manager.create_api(Bookmark, methods=['GET'])
 users = User.query.all()
 bookmarks = Bookmark.query.all()
 
-# @app.after_request
-# def after_request(response):
-#   response.headers.add('Access-Control-Allow-Origin', '*')
-#   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-#   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-#   return response
-
 @app.route("/")
 def hello():
     return "Hello World"
 
-@app.route("/add_bookmark", methods=['POST', 'OPTIONS'])
-@crossdomain(origin='*')
+@app.route("/add_bookmark", methods=['POST'])
+# @crossdomain(origin='*')
 def add_bookmark():
 
     result = request.get_data()
@@ -178,13 +128,6 @@ def add_bookmark():
     # another_bookmark = db.engine.execute("INSERT INTO bookmark VALUES (default, 'now', 'New search 2', 1, 'Yeahh buddy');")
 
     print "Bookmark added"
-    # data = request.get_data()
-    # print data
-    # new_data = json.loads(data)
-    # print new_data
-    # # for key, value in new_data:
-    # #     print key + ": " + value
-
 
 if __name__ == "__main__":
     app.run()
