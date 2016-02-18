dd2145c6e1daea0ccbed447153d4ceed00e6c839
Samuel <samuel.linehan15@gmail.com>
2016-02-17 17:10:01 -0700

    working on deployment

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ heroku = Heroku(app)
 app.config.from_pyfile('config.py')
 # app.config.from_envvar('DATABASE_URL')
 # #Production
-# dotenv_path = join(dirname(__file__), '.env')
-# load_dotenv(dotenv_path)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 db = SQLAlchemy(app)
 
